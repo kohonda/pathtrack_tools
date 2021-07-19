adca83833d89323f0b7775709a2fafea6ba3cba0
kohonda <0905honda@gmail.com>
2021-07-19 14:18:12 +0900

    estimate dy_f from previous few points

--- src/frenet_state_filter/frenet_state_filter.cpp
+++ src/frenet_state_filter/frenet_state_filter.cpp
@@ -4,7 +4,10 @@ namespace pathtrack_tools
 {
     FrenetStateFilter::FrenetStateFilter(const double &sampling_time) : sampling_time_{sampling_time}
     {
-        prev_y_f_ = 0.0;
+        for (size_t i = 0; i < dy_f_queue_size_; i++)
+        {
+            dy_f_queue_.push_back(0.0);
+        }
     }
 
     FrenetStateFilter::~FrenetStateFilter()
@@ -20,12 +23,25 @@ namespace pathtrack_tools
     // TODO : Kalman filter implementation
     double FrenetStateFilter::estimate_dy_f(const double &current_y_f)
     {
-        // Most simple estimation
-        const auto estimated_dy_f = (current_y_f - prev_y_f_) / sampling_time_;
+        dy_f_queue_.pop_front();
 
-        prev_y_f_ = estimated_dy_f;
+        const double current_dy_f = (current_y_f - prev_y_f_) / sampling_time_;
 
-        return estimated_dy_f;
+        dy_f_queue_.push_back(current_dy_f);
+
+        double averaged_dy_f = 0.0;
+        if (dy_f_queue_.size() == dy_f_queue_size_)
+        {
+            averaged_dy_f = std::accumulate(dy_f_queue_.begin(), dy_f_queue_.end(), 0.0) / static_cast<double>(dy_f_queue_size_);
+        }
+        else
+        {
+            std::cerr << "queue size is different" << std::endl;
+        }
+
+        prev_y_f_ = current_y_f;
+
+        return averaged_dy_f;
     }
 
     double FrenetStateFilter::estimate_dy_f(const FrenetCoordinate &pose_f, const Twist &twist) const
--- src/frenet_state_filter/frenet_state_filter.hpp
+++ src/frenet_state_filter/frenet_state_filter.hpp
@@ -2,60 +2,63 @@
 
 #include <iostream>
 #include <cmath>
-#include <Eigen/Dense>
+#include <deque>
+#include <numeric>
 #include "FrenetCoordinate.hpp"
 #include "Pose.hpp"
 #include "Twist.hpp"
 
 namespace pathtrack_tools
 {
-    ///
-    /// @class FrenetStateFilter
-    /// @brief
-    ///
+   ///
+   /// @class FrenetStateFilter
+   /// @brief
+   ///
 
-    class FrenetStateFilter
-    {
-    public:
-        /**
+   class FrenetStateFilter
+   {
+   public:
+      /**
      * @brief Default constructor
      *
      */
-        FrenetStateFilter(const double &sampling_time);
+      FrenetStateFilter(const double &sampling_time);
 
-        /**
+      /**
      * @brief Destroy the FrenetStateFilter object
      *
      */
-        ~FrenetStateFilter();
+      ~FrenetStateFilter();
 
-        /**
+      /**
      * @brief Set the initial pose object
      *
      * @param pose_f
      */
-        void set_initial_pose(const FrenetCoordinate &pose_f, const Twist &twist);
+      void set_initial_pose(const FrenetCoordinate &pose_f, const Twist &twist);
 
-        /**
-     * @brief Estimate dy_f (df in frenet coordinate) from y_f which is converted from observed y_g
+      /**
+     * @brief Estimated from y_f of the past few points
      *
      * @param y_f
-     * @return double
+     * @return double dy_f
      */
-        double estimate_dy_f(const double &current_y_f);
+      double estimate_dy_f(const double &current_y_f);
 
-        /**
+      /**
      * @brief Determined by projection in reference path direction
      *
      * @param current_pose_f
      * @param current_twist
      * @return double
      */
-        double estimate_dy_f(const FrenetCoordinate &current_pose_f, const Twist &current_twist) const;
+      double estimate_dy_f(const FrenetCoordinate &current_pose_f, const Twist &current_twist) const;
 
-    private:
-        const double sampling_time_;
-        double prev_y_f_;
-    };
+   private:
+      const double sampling_time_;
+      double prev_y_f_ = 0.0;
+      const size_t dy_f_queue_size_ = 10;
+      std::deque<double> dy_f_queue_;
+   };
 
 } // namespace pathtrack_tools
